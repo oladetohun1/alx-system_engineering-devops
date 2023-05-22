06dc18951f81537368696d0c3b294e1a1bd3deb9
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-05-22 21:23:45 +0100

    fixed pycodestyle error

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""A script that returns information about an employee TODO list progress, given its ID.  
+"""A script that returns information about an employee TODO list
+progress, given its ID.
 """
 
 import requests
@@ -7,11 +8,12 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    """The script must accept an integer as a parameter, which is the employee ID
+    """The script must accept an integer as a parameter,
+    which is the employee ID
     """
     if len(argv) != 2:
         print('''
-        Only two arguments expected :
+        Only two arguments expected:
         $ ./0-gather_data_from_an_API.py <employee_id>''')
         exit(1)
     id = argv[1]
@@ -21,14 +23,19 @@ if __name__ == "__main__":
 
     # Retrieve employee information
     employee_data = requests.get(employee_url).json()
-    # retrive employee todo lists
+    # Retrieve employee's todo lists
     todo_data = requests.get(todos_url).json()
-    # retrieve other relevant data
+    # Retrieve other relevant data
     employee_name = employee_data['name']
-    completed_tasks = [task['title'] for task in todo_data if task['completed']]
+    completed_tasks = [task['title'] for task in
+                       todo_data if task['completed']]
     num_of_done_tasks = len(completed_tasks)
     total_number_of_tasks = len(todo_data)
-    # print the required output
-    print(f"Employee {employee_name} is done with tasks({num_of_done_tasks}/{total_number_of_tasks}):")
+    # Print the required output
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            employee_name, num_of_done_tasks, total_number_of_tasks
+        )
+    )
     for task in completed_tasks:
         print("\t{}".format(task))
