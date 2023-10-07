3cce2b915e4d818db9b4aabc8629f99cfd9d8e48
Oluwakorede Emmanuel <69650891+oladetohun1@users.noreply.github.com>
2023-10-07 01:33:45 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,60 +1,37 @@
 #!/usr/bin/python3
-"""
-A script that returns information about an employee TODO list
-progress, given its ID and exports its data in CSV format.
+'''
+A Python script to export data in the CSV format.
+'''
 
-Usage:
-    $ ./1-export_to_CSV.py <employee_id>
-"""
+import re  # Import the regular expression module
+import requests  # Import the requests module
+import sys  # Import the sys module
 
-import csv
-import requests
-from sys import argv
+# The API's URL
+API_URL = 'https://jsonplaceholder.typicode.com'
 
-if __name__ == "__main__":
-    """
-    The script must accept an integer as a parameter,
-    which is the employee ID
-    """
-    if len(argv) != 2:
-        print('''
-        Only two arguments expected:
-        $ ./0-gather_data_from_an_API.py <employee_id>
-        ''')
-        exit(1)
-    employee_id = int(argv[1])
-    url = "https://jsonplaceholder.typicode.com"
-    employee_url = f"{url}/users/{employee_id}"
-    todos_url = f"{url}/todos?userId={employee_id}"
+if __name__ == '__main__':
+    # Check if command line arguments were provided
+    if len(sys.argv) > 1:
+        # Check if the argument is a valid user ID
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            # Get the user's information from the API
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
+            todos_res = requests.get('{}/todos'.format(API_URL)).json()
+            user_name = user_res.get('username')
 
-    # Retrieve employee information
-    employee_data = requests.get(employee_url).json()
-    # Retrieve employee's todo lists
-    todo_data = requests.get(todos_url).json()
-    # Retrieve other relevant data
-    employee_id = employee_data.get('id')
-    employee_username = employee_data.get('username')
-    employee_name = employee_data.get('name')
-    completed_tasks = [task for task in todo_data if task.get('completed')]
+            # Filter todos for the given user
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
 
-    # Prepare CSV data
-    csv_data = [
-        [
-            employee_id,
-            employee_username,
-            task.get('completed'),
-            task.get('title')
-        ]
-        for task in completed_tasks
-    ]
-
-    # Export data to CSV file
-    csv_filename = f"{employee_id}.csv"
-    with open(csv_filename, 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(
-            ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-            )
-        csv_writer.writerows(csv_data)
-
-    print(f"Data exported to {csv_filename} successfully.")
+            # Write the data to a CSV file
+            with open('{}.csv'.format(id), 'w') as file:
+                for todo in todos:
+                    file.write(
+                        '"{}","{}","{}","{}"\n'.format(
+                            id,
+                            user_name,
+                            todo.get('completed'),
+                            todo.get('title')
+                        )
+                    )
