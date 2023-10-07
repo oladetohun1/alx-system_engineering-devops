ae4b242f7ce97d99c5c02fc2fed60d2e76937ff9
Oluwakorede Emmanuel <69650891+oladetohun1@users.noreply.github.com>
2023-10-07 01:32:21 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,47 +1,36 @@
 #!/usr/bin/python3
 """
-A script that returns information about an employee TODO list
-progress, given its ID.
-
-Usage:
-    $ ./0-gather_data_from_an_API.py <employee_id>
+This module returns information about his/her TODO list progress.
 """
-
 import requests
 from sys import argv
 
 
-if __name__ == "__main__":
-    """
-    The script must accept an integer as a parameter,
-    which is the employee ID
-    """
-    if len(argv) != 2:
-        print('''
-        Only two arguments expected:
-        $ ./0-gather_data_from_an_API.py <employee_id>
-        ''')
-        exit(1)
-    employee_id = int(argv[1])
-    url = "https://jsonplaceholder.typicode.com"
-    employee_url = f"{url}/users/{employee_id}"
-    todos_url = f"{url}/todos?userId={employee_id}"
+def get_employee_info():
+    """" returns information about his/her TODO list progress. """
 
-    # Retrieve employee information
-    employee_data = requests.get(employee_url).json()
-    # Retrieve employee's todo lists
-    todo_data = requests.get(todos_url).json()
+    emp_id = int(argv[1])
+    url_1 = "https://jsonplaceholder.typicode.com/todos/"
+    url_2 = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
+    todo_response = requests.get(url_1).json()
+    user_response = requests.get(url_2).json()
 
-    # Retrieve other relevant data
-    employee_name = employee_data.get('name')
-    completed_tasks = [task['title'] for task in todo_data
-                       if task.get('completed')]
-    num_of_done_tasks = len(completed_tasks)
-    total_number_of_tasks = len(todo_data)
+    task_done = 0
+    total_task = 0
+    task_title = ""
+    Employee_name = user_response["name"]
 
-    # Print the required output
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, num_of_done_tasks, total_number_of_tasks
-    ))
-    for task in completed_tasks:
-        print("\t{}".format(task))
+    for response in todo_response:
+        if response["userId"] is emp_id:
+            if response["completed"] is True:
+                task_done += 1
+                task_title += f"\t {response['title']}\n"
+            total_task += 1
+
+    print(f"""Employee {Employee_name} is done \
+with tasks({task_done}/{total_task}):
+{task_title}""", end="")
+
+
+if __name__ == '__main__':
+    get_employee_info()
