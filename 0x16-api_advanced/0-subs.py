7ffb9c26263b8a6d6c7e37d41958684ad975bebc
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-10-11 16:18:31 +0100

    error free now

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-Write a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.
+Write a function that queries the Reddit API and returns the number of
+subscribers (not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0.
 """
 import requests
 
@@ -28,5 +30,6 @@ def number_of_subscribers(subreddit):
     else:
         # Handle other errors
         print(
-            f"Error: {response.status_code} - Unable to retrieve data for subreddit '{subreddit}'")
+            f"Error: {response.status_code} - Unable to retrieve data for\
+                    subreddit '{subreddit}'")
         return 0
